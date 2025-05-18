6a6b24282c7b1550f9e3d296107bb5f26461aa88
AlexanderF02 <alexander.fallstrom5@gmail.com>
2025-05-18 22:03:47 +0200

    app to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { ThemeProvider } from "@/components/ThemeProvider"; // Ensure correct path
-import { Toaster } from "./components/ui/toaster.js";
-import { Toaster as Sonner } from "./components/ui/sonner.js";
-import { TooltipProvider } from "./components/ui/tooltip.js";
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index.js";
+import Index from "./pages/Index.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/register.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Savedcsv from "./pages/Savedcsv.jsx";
-import Editdashboard from './pages/Editdashboard.jsx';
+import Editdashboard from './pages/Editdashboard.jsx'; 
 
 const queryClient = new QueryClient();
 
